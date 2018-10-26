d8b3799f3921ad91b4d46c9fc95adcb4cc72e989
Tarru <reinboldvalentin@hotmail.fr>
2018-10-26 11:38:59 +0200

    Update Calculator/main.c

--- Calculator/main.c
+++ Calculator/main.c
@@ -5,9 +5,12 @@ int addition(int a , int b)
 {
     return a+b;
 }
+int sous (int a, int b)
+
 
 int main ()
 {
     printf("a+b= %d\n", addition(1,8));
+    printf ("a-b=%d\n",sous(1,4))
     return 0;
 }
