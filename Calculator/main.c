0496a045f86935a18f23ed242eda8f62a5713fe5
Tarru <reinboldvalentin@hotmail.fr>
2018-10-26 13:37:51 +0200

    Update Calculator/main.c

--- Calculator/main.c
+++ Calculator/main.c
@@ -12,10 +12,7 @@ int main ()
 {
     printf("a+b= %d\n", addition(2,8));
     printf ("a-b=%d\n",sous(5,4))
-<<<<<<< HEAD
     printf("chatte\n");
-=======
-    printf("p�nis\n")
->>>>>>> a458492a062d3168511cdd944d3ab4ab1fba9301
+
     return 0;
 }
